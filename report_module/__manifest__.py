23037e1c8e175ebb14a26ee1cffc679bc9936f11
Tiago-M-Andrade <x15_tiago@hotmail.com>
2023-10-31 01:58:24 -0300

    first commit

---
+++ report_module/__manifest__.py
@@ -0,0 +1,8 @@
+{
+  'name': 'Report Module',
+  'version': '1.0',
+  'description': 'My first report module made in odoo.',
+  'summary': 'Module for learning purposes.',
+  'author': 'Tiago Martins Andrade',
+  'website': 'https://www.linkedin.com/in/tiago-martins-andrade/',
+}
---
+++ report_module/models/report_model.py
@@ -0,0 +1,5 @@
+from odoo import api, models, fields
+
+class ReportModel(models.Model):
+  _name = 'report.model'
+  _description = 'Model used do make reports in odoo.'
